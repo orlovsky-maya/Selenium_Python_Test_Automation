14350b1a2f86ac9e91770630459b0f815a681dc6
maya <orlovky-maya@mail.ru>
2023-04-19 17:07:55 +0400

    Added solutions for third part of course.
    4_Testing with PyTest:
     * test_abs_project
     * test_fixture1
     * test_fixture2
     * test_fixture3
     * test_fixture5
     * test_fixture_autouse
     * test_pytest_output

---
+++ 4_Testing_with_PyTest/test_pytest_output.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--remote-debugging-port=9515")
+browser = webdriver.Chrome(options=chrome_options)
+
+
+def test_part1():
+    link = "http://suninjuly.github.io/registration1.html"
+    try:
+        browser.get(link)
+
+        input_first_name = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
+        input_first_name.send_keys('Maya')
+        input_last_name = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
+        input_last_name.send_keys('Orlovskaya')
+        input_email = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
+        input_email.send_keys('orlovsky-maya@mail.ru')
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assertEquals проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        result_text = "Congratulations! You have successfully registered!"
+        assert result_text == welcome_text, "incorrect registration text"
+
+    finally:
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(1)
+        # закрываем браузер после всех манипуляций
+        # browser.quit()
+
+
+def test_part2():
+    link = "http://suninjuly.github.io/registration2.html"
+    try:
+        browser.get(link)
+
+        input_first_name = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
+        input_first_name.send_keys('Maya')
+        input_last_name = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
+        input_last_name.send_keys('Orlovskaya')
+        input_email = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
+        input_email.send_keys('orlovsky-maya@mail.ru')
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assertEquals проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        result_text = "Congratulations! You have successfully registered!"
+        assert result_text == welcome_text, "incorrect registration text"
+
+    finally:
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(1)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
